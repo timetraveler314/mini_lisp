5f858d06a029220cec36584fa1ea67f682b4d2f4
timetraveler314 <36299842+timetraveler314@users.noreply.github.com>
2024-05-03 16:37:43 +0800

    Finished Lv1. data values

---
+++ src/value.cpp
@@ -0,0 +1,72 @@
+//
+// Created by timetraveler314 on 5/3/24.
+//
+
+#include "value.h"
+
+#include <cmath>
+#include <iomanip>
+
+#include "error.h"
+
+/**
+ * @brief 将 NumericValue 转换为字符串表示形式
+ *
+ * 将 NumericValue
+ * 对象转换为字符串表示形式，如果该值为整数，则返回整数部分的字符串形式，
+ * 否则返回浮点数本身的字符串形式。
+ *
+ * @return 转换后的字符串
+ *
+ * @note 这里采用`std::modf`函数来判断一个浮点数的小数部分是否为0。
+ */
+template <>
+std::string NumericValue::toString() const {
+    double intpart;
+    return std::modf(value, &intpart) == 0.0 ? std::to_string(std::lround(intpart)) : std::to_string(value);
+}
+
+template<>
+std::string BooleanValue::toString() const {
+    return value ? "#t" : "#f";
+}
+
+template<>
+std::string StringValue::toString() const {
+    std::ostringstream ss;
+    ss << std::quoted(value);
+    return ss.str();
+}
+
+template<>
+std::string SymbolValue::toString() const {
+    return value;
+}
+
+std::string NilValue::toString() const {
+    return "()";
+}
+
+std::string PairValue::toString() const {
+    std::ostringstream ss;
+    ss << "(";
+    auto current = this;
+
+    while (current) {
+        ss << current->car->toString();
+        if (auto pair = dynamic_cast<const PairValue*>(current->cdr.get())) {
+            ss << " ";
+            current = pair;
+        } else {
+            if (auto nil = dynamic_cast<const NilValue*>(current->cdr.get())) {
+                ss << ")";
+                current = nullptr;
+            } else {
+                ss << " . " << current->cdr->toString() << ")";
+                current = nullptr;
+            }
+        }
+    }
+
+    return ss.str();
+}
---
+++ src/value.h
@@ -0,0 +1,89 @@
+//
+// Created by timetraveler314 on 5/3/24.
+//
+
+#ifndef MINI_LISP_VALUE_H
+#define MINI_LISP_VALUE_H
+
+#include <functional>
+#include <memory>
+#include <optional>
+#include <utility>
+#include <vector>
+
+enum class ValueType {
+    BOOLEAN_VALUE,
+    NUMERIC_VALUE,
+    STRING_VALUE,
+    NIL_VALUE,
+    SYMBOL_VALUE,
+    PAIR_VALUE,
+};
+
+class Value;
+using ValuePtr = std::shared_ptr<Value>;
+
+class Value {
+    ValueType type;
+
+protected:
+    explicit Value(ValueType type): type{type} {}
+
+public:
+    virtual ~Value() = default;
+
+    ValueType getType() const {
+        return type;
+    }
+
+    virtual std::string toString() const = 0;
+};
+
+template<ValueType value_type, typename T>
+class PrimitiveValue;
+
+using BooleanValue = PrimitiveValue<ValueType::BOOLEAN_VALUE, bool>;
+using NumericValue = PrimitiveValue<ValueType::NUMERIC_VALUE, double>;
+using StringValue = PrimitiveValue<ValueType::STRING_VALUE, std::string>;
+using SymbolValue = PrimitiveValue<ValueType::SYMBOL_VALUE, std::string>;
+
+template<ValueType value_type, typename T>
+class PrimitiveValue : public Value {
+    T value;
+
+public:
+    explicit PrimitiveValue(T value): Value(value_type), value{value} {}
+
+    T getValue() const {
+        return value;
+    }
+
+    std::string toString() const override;
+};
+
+class NilValue : public Value {
+public:
+    NilValue(): Value(ValueType::NIL_VALUE) {}
+
+    std::string toString() const override;
+};
+
+class PairValue : public Value {
+    ValuePtr car;
+    ValuePtr cdr;
+
+public:
+    PairValue(const ValuePtr& car, const ValuePtr& cdr): Value(ValueType::PAIR_VALUE), car{car}, cdr{cdr} {}
+
+    ValuePtr getCar() const {
+        return car;
+    }
+
+    ValuePtr getCdr() const {
+        return cdr;
+    }
+
+    std::string toString() const override;
+};
+
+#endif //MINI_LISP_VALUE_H
